60ed4c9f2e08dbcd18074d69fec83f727924a6bc
kailash <kailashdewasi620@gmail.com>
2025-04-04 20:25:58 +0530

    test12

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import http from "http";
 import express from 'express';
 const app = express();
-const server = http.createServer(app);
 app.get("/",(req,res) => {
     res.send("This is home page");
 })
 
-server.listen(8080,() => {
-    console.log("check");
+app.listen(8080,() => {
+    console.log("Server running");
 })
